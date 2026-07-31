25c965dca41c6dcbba7692c8ad505b37abf6cbd3
César Pizano <cpizano@fisoluciones.com>
2026-07-31 09:40:47 -0500

    reload 20260731 t0

--- site/index.php
+++ site/index.php
@@ -2,7 +2,7 @@
 $dir = "/var/www/html/xlsx/";
 $server_ip = trim(file_get_contents('https://checkip.amazonaws.com'));
 echo "Internal Server IP: " . $server_ip . "<br/>";
-$xlsx_files = array_diff(scandir($dir, SCANDIR_SORT_DESCENDING), array('..', '.'));
+/*$xlsx_files = array_diff(scandir($dir, SCANDIR_SORT_DESCENDING), array('..', '.'));
 foreach  ($xlsx_files as $value) {
     echo "<a href='http://" . $server_ip . "/xlsx/" . $value . "'>" . $value ."</a><br/>";
-}
+}*/
---
+++ site/reload.php
@@ -0,0 +1,6 @@
+<?php
+
+$y = filter_input(INPUT_GET, "y");
+$m = filter_input(INPUT_GET, "y");
+
+echo "Año: " . $y . " Mes: " . $m;
