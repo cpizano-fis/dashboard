df15ac8961a0165572c97e9ad8dac3b1415ea2c4
César Pizano <cpizano@fisoluciones.com>
2026-07-31 10:47:21 -0500

    exe python 20260731 t1

--- site/reload.php
+++ site/reload.php
@@ -5,9 +5,13 @@ $m = filter_input(INPUT_GET, "m");
 $output = [];
 $status_code = 0;
 // The array will populate with each line of output
-exec('python3 /home/ubuntu/dashboard/cron/db-cron.py ECUAINTEGRAL 0 ' . $y . ' ' . $m, $output, $status_code);
+$command = 'python3 /home/ubuntu/dashboard/cron/db-cron.py ECUAINTEGRAL 0 ' . $y . ' ' . $m;
+echo $command . "<br>";
+exec($command, $output, $status_code);
+$salida = print_r($output, TRUE);
 if ($status_code === 0) {
     echo "Command succeeded!";
 } else {
-    echo "Command failed with code: " . $status_code;
+	echo $salida;
+    echo "<br>Command failed with code: " . $status_code;
 }
