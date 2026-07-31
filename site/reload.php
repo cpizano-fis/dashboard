6bf4605141f15b928236a9ecfdd1c1a2ca535106
César Pizano <cpizano@fisoluciones.com>
2026-07-31 10:43:40 -0500

    exe python 20260731 t0

--- site/reload.php
+++ site/reload.php
@@ -2,5 +2,12 @@
 
 $y = filter_input(INPUT_GET, "y");
 $m = filter_input(INPUT_GET, "m");
-
-echo "Año: " . $y . " Mes: " . $m;
+$output = [];
+$status_code = 0;
+// The array will populate with each line of output
+exec('python3 /home/ubuntu/dashboard/cron/db-cron.py ECUAINTEGRAL 0 ' . $y . ' ' . $m, $output, $status_code);
+if ($status_code === 0) {
+    echo "Command succeeded!";
+} else {
+    echo "Command failed with code: " . $status_code;
+}
